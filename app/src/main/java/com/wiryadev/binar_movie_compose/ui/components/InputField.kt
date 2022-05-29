427638e21d79fb565cc06d80017c07e7ac0ecf1d
wiryadev <abrarwiryawan@gmail.com>
2022-05-29 17:52:56 +0700

    create TextField

---
+++ app/src/main/java/com/wiryadev/binar_movie_compose/ui/components/InputField.kt
@@ -0,0 +1,137 @@
+package com.wiryadev.binar_movie_compose.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.wiryadev.binar_movie_compose.R
+
+@Composable
+fun Email(
+    emailState: TextFieldState = remember { EmailState() },
+    imeAction: ImeAction = ImeAction.Next,
+    onImeAction: () -> Unit = {}
+) {
+    OutlinedTextField(
+        value = emailState.text,
+        onValueChange = {
+            emailState.text = it
+        },
+        label = {
+            Text(
+                text = stringResource(id = R.string.email),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                emailState.onFocusChange(focusState.isFocused)
+                if (!focusState.isFocused) {
+                    emailState.enableShowErrors()
+                }
+            },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        isError = emailState.showErrors(),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                onImeAction()
+            }
+        )
+    )
+
+    emailState.getError()?.let { error -> TextFieldError(textError = error) }
+}
+
+@Composable
+fun Password(
+    label: String,
+    passwordState: TextFieldState,
+    modifier: Modifier = Modifier,
+    imeAction: ImeAction = ImeAction.Done,
+    onImeAction: () -> Unit = {}
+) {
+    val showPassword = remember { mutableStateOf(false) }
+    OutlinedTextField(
+        value = passwordState.text,
+        onValueChange = {
+            passwordState.text = it
+            passwordState.enableShowErrors()
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                passwordState.onFocusChange(focusState.isFocused)
+                if (!focusState.isFocused) {
+                    passwordState.enableShowErrors()
+                }
+            },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        label = {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        },
+        trailingIcon = {
+            if (showPassword.value) {
+                IconButton(onClick = { showPassword.value = false }) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = stringResource(id = R.string.hide_password)
+                    )
+                }
+            } else {
+                IconButton(onClick = { showPassword.value = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = stringResource(id = R.string.show_password)
+                    )
+                }
+            }
+        },
+        visualTransformation = if (showPassword.value) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        isError = passwordState.showErrors(),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                onImeAction()
+            }
+        )
+    )
+
+    passwordState.getError()?.let { error -> TextFieldError(textError = error) }
+}
+
+@Composable
+fun TextFieldError(textError: String) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = textError,
+            modifier = Modifier.fillMaxWidth(),
+            style = LocalTextStyle.current.copy(color = MaterialTheme.colorScheme.error)
+        )
+    }
+}
---
+++ app/src/main/java/com/wiryadev/binar_movie_compose/ui/components/TextFieldState.kt
@@ -0,0 +1,98 @@
+package com.wiryadev.binar_movie_compose.ui.components
+
+import android.util.Patterns
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+open class TextFieldState(
+    private val validator: (String) -> Boolean = { true },
+    private val errorFor: (String) -> String = { "" }
+) {
+    var text: String by mutableStateOf("")
+
+    // was the TextField ever focused
+    var isFocusedDirty: Boolean by mutableStateOf(false)
+    var isFocused: Boolean by mutableStateOf(false)
+    private var displayErrors: Boolean by mutableStateOf(false)
+
+    open val isValid: Boolean
+        get() = validator(text)
+
+    fun onFocusChange(focused: Boolean) {
+        isFocused = focused
+        if (focused) isFocusedDirty = true
+    }
+
+    fun enableShowErrors() {
+        // only show errors if the text was at least once focused
+        if (isFocusedDirty) {
+            displayErrors = true
+        }
+    }
+
+    fun showErrors() = !isValid && displayErrors
+
+    open fun getError(): String? {
+        return if (showErrors()) {
+            errorFor(text)
+        } else {
+            null
+        }
+    }
+}
+
+/**
+ * State for Email TextField
+ */
+class EmailState : TextFieldState(
+    validator = ::isEmailValid,
+    errorFor = ::emailValidationError,
+)
+
+// Returns an error to be displayed or null if no error was found
+private fun emailValidationError(email: String): String {
+    return "Invalid email: $email"
+}
+
+private fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+/**
+ * State for Password and ConfirmPassword TextField
+ */
+class PasswordState : TextFieldState(
+    validator = ::isPasswordValid,
+    errorFor = ::passwordValidationError,
+)
+
+class ConfirmPasswordState(private val passwordState: PasswordState) : TextFieldState() {
+    override val isValid
+        get() = passwordAndConfirmationValid(passwordState.text, text)
+
+    override fun getError(): String? {
+        return if (showErrors()) {
+            passwordConfirmationError()
+        } else {
+            null
+        }
+    }
+}
+
+private fun passwordAndConfirmationValid(password: String, confirmedPassword: String): Boolean {
+    return isPasswordValid(password) && password == confirmedPassword
+}
+
+private fun isPasswordValid(password: String): Boolean {
+    return password.length > 6
+}
+
+@Suppress("UNUSED_PARAMETER")
+private fun passwordValidationError(password: String): String {
+    return "Invalid password"
+}
+
+private fun passwordConfirmationError(): String {
+    return "Passwords don't match"
+}
