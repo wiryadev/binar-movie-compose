c19f4b9c62a546e5ddfe2a6db30fb9bd54acaca6
Abrar Wiryawan <abrarwiryawan@gmail.com>
2022-06-03 00:00:29 +0700

    add splashscreen

--- app/src/main/java/com/wiryadev/binar_movie_compose/MainActivity.kt
+++ app/src/main/java/com/wiryadev/binar_movie_compose/MainActivity.kt
@@ -3,13 +3,14 @@ package com.wiryadev.binar_movie_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.wiryadev.binar_movie_compose.ui.BinarMovieApp
 import com.wiryadev.binar_movie_compose.ui.MainViewModel
 import com.wiryadev.binar_movie_compose.ui.theme.BinarMovieComposeTheme
@@ -19,10 +20,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
-            val viewModel: MainViewModel = viewModel()
             val isLoggedIn by viewModel.isLoggedIn.collectAsState()
             viewModel.getUser()
             BinarMovieComposeTheme {
